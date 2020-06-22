0d4d86cb0daa0fd40d952b92cb8afad21cf4b169
Dalmir da Silva <dalmirdasilva@gmail.com>
2020-06-21 23:42:48 -0700

    Update def for custom pins.

--- src/WiredDevice/WiredDevice.cpp
+++ src/WiredDevice/WiredDevice.cpp
@@ -10,7 +10,7 @@ WiredDevice::WiredDevice(uint8_t deviceAddress)
 
 WiredDevice::WiredDevice(uint8_t sdaPin, uint8_t sclPin, uint8_t deviceAddress) : deviceAddress(deviceAddress) {
 
-#ifdef TARGET_ESP8266
+#ifdef CUSTOM_WIRE_PINS_SUPPORT_ENABLED
 
   // Begins the i2c module for esp8266 using custom pins.
   Wire.begin(sdaPin, sclPin);
